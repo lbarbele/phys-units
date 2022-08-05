33c9c61f06e7400453b5e9f44860d63a611db308
Luan Bonneau Arbeletche <luan.arbeletche@gmail.com>
2022-08-05 02:27:12 -0300

    change include guard

--- include/units/units.h
+++ include/units/units.h
@@ -1,5 +1,5 @@
-#ifndef _include_units_h
-#define _include_units_h
+#ifndef _include_units_units_h
+#define _include_units_units_h
 
 #include <units/details/ratio.h>
 #include <units/details/traits.h>
