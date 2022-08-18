6545bda619084edc73863f9f342eea413d51647a
Luan Bonneau Arbeletche <luan.arbeletche@gmail.com>
2022-08-18 17:19:21 -0300

    macro for the definition of base units

--- include/units/units.h
+++ include/units/units.h
@@ -9,17 +9,16 @@
 #include <units/details/traits.h>
 #include <units/details/unit.h>
 
-#define units_add_base_unit(id, name_singular, name_plural, abbreviation) \
-  using name_singular = make_unit<base_unit<id>>; \
-  using name_plural = name_singular; \
-  using abbreviation = name_singular; \
-  template<> constexpr inline std::string_view units::_details::name<name_singular> = #name_singular; \
-  template<> constexpr inline std::string_view units::_details::plural<name_singular> = #name_plural; \
-  template<> constexpr inline std::string_view units::_details::abbrev<name_singular> = #abbreviation;
+#define units_set_name(_unit_, _name_) template <> constexpr inline auto ::units::_details::_unit::unit_name<_unit_> = std::string_view(#_name_)
+#define units_set_symbol(_unit_, _symbol_) template <> constexpr inline auto ::units::_details::_unit::unit_symbol<_unit_> = std::string_view(#_symbol_)
+
+#define units_add_base_unit(_id_, _name_, _symbol_) \
+  using _name_ = ::units::_details::base_unit<_id_>; \
+  using _symbol_ = _name_; \
+  units_set_name(_name_, _name_); \
+  units_set_symbol(_name_, _symbol_);
 
 namespace units {
-
-  using lala = double;
 
   // - imports
   using _details::ratio;
@@ -29,21 +28,20 @@ namespace units {
   using _details::squared;
   using _details::inverse_squared;
 
-  using _details::base_unit;
   using _details::make_unit;
 
   using _details::quantity_t;
 
   // - base units
-  units_add_base_unit(0, meter, meters, m);
-  units_add_base_unit(1, second, seconds, s);
-  units_add_base_unit(2, kilogram, kilograms, kg);
-  units_add_base_unit(3, ampere, amperes, A);
-  units_add_base_unit(4, kelvin, kelvins, K);
-  units_add_base_unit(5, mole, moles, mol);
-  units_add_base_unit(6, candela, candelas, cd);
-  units_add_base_unit(7, radian, radians, rad);
-  units_add_base_unit(8, steradian, steradians, sr);
+  units_add_base_unit(0, meter, m);
+  units_add_base_unit(1, second, s);
+  units_add_base_unit(2, kilogram, kg);
+  units_add_base_unit(3, ampere, A);
+  units_add_base_unit(4, kelvin, K);
+  units_add_base_unit(5, mole, mol);
+  units_add_base_unit(6, candela, cd);
+  units_add_base_unit(7, radian, rad);
+  units_add_base_unit(8, steradian, sr);
 
   // - multiples of base units
   using nanometer = make_unit<std::nano, m>;
@@ -58,14 +56,6 @@ namespace units {
   using volt    = make_unit<joule, inverse<coulomb>>;
   using farad   = make_unit<coulomb, inverse<volt>>;
   using ohm     = make_unit<volt, inverse<ampere>>;
-  // ...
-
-  using gram = make_unit<ratio<1, 1000>, kilogram>;
-  using centimeter = make_unit<ratio<1, 100>, meter>;
-
-  using gram_per_squared_centimeter = make_unit<gram, inverse<centimeter>>;
-
-  using depth_t = quantity_t<gram_per_squared_centimeter>;
 }
 
 #endif
