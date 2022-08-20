269d24d762318f691d96c724bb2cb1b5a5cbe7d3
Luan Bonneau Arbeletche <luan.arbeletche@gmail.com>
2022-08-20 15:26:12 -0300

    arithmetic concept

--- include/units/details/traits.h
+++ include/units/details/traits.h
@@ -1,6 +1,7 @@
 #ifndef _include_units_details_traits_h
 #define _include_units_details_traits_h
 
+#include <concepts>
 #include <cstdint>
 #include <type_traits>
 #include <ratio>
@@ -18,6 +19,11 @@ namespace units::_details::props {
   struct indexed {
     static constexpr auto index = i;
   };
+}
+
+namespace units::_details::concepts {
+  template <class T>
+  concept arithmetic = std::is_integral_v<T> || std::is_floating_point_v<T>;
 }
 
 namespace units::_details::traits {
