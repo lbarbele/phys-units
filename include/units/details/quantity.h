f0aeb444332815eaf67c168f562f71c0f4424f3d
Luan Bonneau Arbeletche <luan.arbeletche@gmail.com>
2022-08-22 12:10:39 -0300

    modulo operation and some improvements

--- include/units/details/quantity.h
+++ include/units/details/quantity.h
@@ -27,6 +27,15 @@ namespace units::_details {
 
     template <class T>
     constexpr inline bool is_quantity_v = is_quantity<T>::value;
+
+    template <class T>
+    struct is_integral_quantity : std::false_type {};
+
+    template <req::unit U, std::integral V>
+    struct is_integral_quantity<quantity<U, V>> : std::true_type {};
+
+    template <class Q>
+    constexpr inline bool is_integral_quantity_v = is_integral_quantity<Q>::value;
   }
 
   // - concept of a quantity
@@ -34,6 +43,12 @@ namespace units::_details {
   namespace req {
     template <class T>
     concept quantity = traits::is_quantity_v<T>;
+
+    template <class T, class U>
+    concept quantity_compatible = traits::is_compatible_unit_v<typename T::unit_type, typename U::unit_type>;
+
+    template <class Q>
+    concept integral_quantity = traits::is_integral_quantity_v<Q>;
   }
 
   // - definition of the quantity class
@@ -42,8 +57,8 @@ namespace units::_details {
   struct quantity {
   public:
     using type = quantity;
-    using unit_type = Unit;
-    using value_type = ValueType;
+    using unit_type = std::decay_t<Unit>;
+    using value_type = std::decay_t<ValueType>;
 
   private:
     value_type m_value;
@@ -53,28 +68,32 @@ namespace units::_details {
 
     // * access to raw underlying value
 
-    constexpr const value_type& get_value() const {
+    constexpr auto& get_value() {
       return m_value;
     }
 
-    constexpr type& set_value(const req::arithmetic auto& value) {
+    constexpr const auto& get_value() const {
+      return m_value;
+    }
+
+    constexpr type& set_value(const req::arithmetic auto value) {
       m_value = value;
       return *this;
     }
 
-    constexpr type set_value(const req::arithmetic auto& value) const {
+    constexpr type set_value(const req::arithmetic auto value) const {
       return type(value);
     }
 
     // * unit (type) conversion
 
-    template <req::compatible_units<unit_type> U, req::arithmetic V>
+    template <req::unit_compatible<unit_type> U, req::arithmetic V = value_type>
     constexpr auto convert() const {
       using factor = ratio_divide<typename unit_type::factor, typename U::factor>;
       return quantity<U, V>(V(m_value) * V(factor::num)/V(factor::den));
     }
 
-    template <req::compatible_units<unit_type> U, req::arithmetic V>
+    template <req::unit_compatible<unit_type> U, req::arithmetic V>
     constexpr operator quantity<U, V>() const {
       return convert<U, V>();
     }
@@ -88,66 +107,58 @@ namespace units::_details {
     // * assignment operations for quantities of compatible units
 
     // simple assignment
-    constexpr type& operator=(const req::quantity auto& other) {
-      return set_value(type(other).get_value());
+    constexpr type& operator=(const req::quantity auto q) {
+      return set_value(type(q).get_value());
     }
 
     // addition assignment
-    constexpr type& operator+=(const req::quantity auto& other) {
-      m_value += type(other).get_value();
-      return *this;
+    constexpr type& operator+=(const req::quantity auto q) {
+      return set_value(get_value() + type(q).get_value());
     }
 
     // subtraction assignment
-    constexpr type& operator-=(const req::quantity auto& other) {
-      m_value -= type(other).get_value();
-      return *this;
+    constexpr type& operator-=(const req::quantity auto q) {
+      return set_value(get_value() - type(q).get_value());
     }
 
     // multiplication assignment by dimensionless object
-    constexpr type& operator*=(const std::convertible_to<value_type> auto& m) {
-      m_value *= value_type(m);
-      return *this;
+    constexpr type& operator*=(const std::convertible_to<value_type> auto x) {
+      return set_value(get_value() * value_type(x));
     }
 
     // division assignment by dimensionless object
-    constexpr type& operator/=(const std::convertible_to<value_type> auto& m) {
-      m_value /= value_type(m);
-      return *this;
+    constexpr type& operator/=(const std::convertible_to<value_type> auto x) {
+      return set_value(get_value() / value_type(x));
     }
 
     // * assignment operations for dimensionless quantities
 
     // simple assignment
-    constexpr type& operator=(const req::arithmetic auto& value)
+    constexpr type& operator=(const std::convertible_to<value_type> auto& x)
     requires req::dimensionless_unit<unit_type> {
-      return set_value(value);
+      return set_value(value_type(x));
     }
 
     // addition assignemnt
-    constexpr type& operator+=(const req::arithmetic auto& value)
+    constexpr type& operator+=(const std::convertible_to<value_type> auto& x)
     requires req::dimensionless_unit<unit_type> {
-      m_value += value;
-      return *this;
+      return set_value(get_value() + value_type(x));
     }
 
     // subtraction assignment
-    constexpr type& operator-=(const req::arithmetic auto& value)
+    constexpr type& operator-=(const std::convertible_to<value_type> auto& x)
     requires req::dimensionless_unit<unit_type> {
-      m_value -= value;
-      return *this;
+      return set_value(get_value() - value_type(x));
     }
 
     // * increment/decrement operations
 
     constexpr type& operator++() {
-      ++m_value;
-      return *this;
+      return set_value(++m_value);
     }
 
     constexpr type& operator--() {
-      --m_value;
-      return *this;
+      return set_value(--m_value);
     }
 
     constexpr type operator++(int) {
@@ -170,85 +181,86 @@ namespace units::_details {
       return type(-get_value());
     };
 
-    // addition
-    template <req::compatible_units<unit_type> U, class V>
-    constexpr auto operator+(const quantity<U, V>& other) const {
-      using res_t = quantity<unit_type, decltype(value_type{}+V{})>;
-      auto value = res_t(*this).get_value() + res_t(other).get_value();
-      return res_t(value);
-    }
-
-    // subtraction
-    template <req::compatible_units<unit_type> U, class V>
-    constexpr auto operator-(const quantity<U, V>& other) const {
-      using res_t = quantity<unit_type, decltype(value_type{}-V{})>;
-      auto value = res_t(*this).get_value() - res_t(other).get_value();
-      return res_t(value);
-    }
-
-    // multiplication
-    template <class U, class V>
-    constexpr auto operator*(const quantity<U, V>& other) const {
-      auto value = get_value() * other.get_value();
-      using res_t = quantity<make_unit<unit_type, U>, decltype(value)>;
-      return res_t(value);
-    }
-
-    // division
-    template <class U, class V>
-    constexpr auto operator/(const quantity<U, V>& other) const {
-      auto value = get_value() / other.get_value();
-      using res_t = quantity<make_unit<unit_type, inverse<U>>, decltype(value)>;
-      return res_t(value);
-    }
-
-    // multiplication by dimensionless quantity
-    constexpr auto operator*(const req::arithmetic auto& x) {
-      auto value = get_value() * x;
+    // addition with quantity of compatible unit
+    constexpr auto operator+(const req::quantity_compatible<type> auto q) const {
+      const auto value = get_value() + q.template convert<unit_type>().get_value();
       return quantity<unit_type, decltype(value)>(value);
     }
 
-    // division by dimensionless quantity
-    constexpr auto operator/(const req::arithmetic auto& x) {
-      auto value = get_value() / x;
+    // subtraction with quantity of compatible unit
+    constexpr auto operator-(const req::quantity_compatible<type> auto q) const {
+      const auto value = get_value() - q.template convert<unit_type>().get_value();
       return quantity<unit_type, decltype(value)>(value);
+    }
+
+    // multiplication by another quantity
+    constexpr auto operator*(const req::quantity auto q) const {
+      using resulting_unit = make_unit<unit_type, typename decltype(q)::unit_type>;
+      const auto value = get_value() * q.get_value();
+      return quantity<resulting_unit, decltype(value)>(value);
+    }
+
+    // division by another quantity
+    constexpr auto operator/(const req::quantity auto q) const {
+      using resulting_unit = make_unit<unit_type, inverse<typename decltype(q)::unit_type>>;
+      const auto value = get_value() / q.get_value();
+      return quantity<resulting_unit, decltype(value)>(value);
+    }
+
+    // multiplication by arithmetic type
+    constexpr auto operator*(const req::arithmetic auto x) const {
+      return quantity<unit_type, decltype(get_value()*x)>(get_value()*x);
+    }
+
+    // division by arithmetic type
+    constexpr auto operator/(const req::arithmetic auto x) const {
+      return quantity<unit_type, decltype(get_value()/x)>(get_value()/x);
+    }
+
+    // modulo operation by integral type (onlu for integral quantities)
+    constexpr auto operator%(const std::integral auto i) const
+    requires req::integral_quantity<type> {
+      return decltype(*this/i)(get_value()%i);
+    }
+
+    // modulo operation by another integral quantity
+    constexpr auto operator%(const req::integral_quantity auto q) const
+    requires req::integral_quantity<type> {
+      return decltype(*this/q)(get_value() % q.get_value());
     }
 
     // * comparison
 
-    constexpr bool operator==(const req::quantity auto& other) const {
-      return get_value() == type(other).get_value();
+    constexpr bool operator==(const req::quantity auto q) const {
+      return get_value() == type(q).get_value();
     }
 
-    constexpr bool operator!=(const req::quantity auto& other) const {
-      return get_value() != type(other).get_value();
+    constexpr bool operator!=(const req::quantity auto q) const {
+      return get_value() != type(q).get_value();
     }
 
-    constexpr bool operator<(const req::quantity auto& other) const {
-      return get_value() < type(other).get_value();
+    constexpr bool operator<(const req::quantity auto q) const {
+      return get_value() < type(q).get_value();
     }
 
-    constexpr bool operator>(const req::quantity auto& other) const {
-      return get_value() > type(other).get_value();
+    constexpr bool operator>(const req::quantity auto q) const {
+      return get_value() > type(q).get_value();
     }
 
-    constexpr bool operator<=(const req::quantity auto& other) const {
-      return get_value() <= type(other).get_value();
+    constexpr bool operator<=(const req::quantity auto q) const {
+      return get_value() <= type(q).get_value();
     }
 
-    constexpr bool operator>=(const req::quantity auto& other) const {
-      return get_value() >= type(other).get_value();
+    constexpr bool operator>=(const req::quantity auto q) const {
+      return get_value() >= type(q).get_value();
     }
 
   };
 
-  // - multiplication by dimensionless from lhs
+  // - multiplication by arithmetic type from lhs
 
-  constexpr auto operator*(const req::arithmetic auto& x, const req::quantity auto& q) {
-    auto value = q.get_value() * x;
-    using unit_type = typename std::decay_t<decltype(q)>::unit_type;
-    using value_type = decltype(value);
-    return quantity<unit_type, value_type>(value);
+  constexpr auto operator*(const req::arithmetic auto x, const req::quantity auto q) {
+    return q * x;
   };
 
   // - print function
